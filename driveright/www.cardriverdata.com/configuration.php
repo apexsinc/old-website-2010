9e545ab4d5099fd32f24d78f4779b2cdfc4ccd82
Jaymar Anasco <46880335+jaymaranasco@users.noreply.github.com>
2021-08-20 11:27:53 +0800

    archived

---
+++ driveright/www.cardriverdata.com/configuration.php
@@ -0,0 +1,65 @@
+<?php
+class JConfig {
+/* Site Settings */
+var $offline = '0';
+var $offline_message = 'This site is down for development.<br /> Please check back again soon.';
+var $sitename = 'www.cardriverdata.com';
+var $editor = 'tinymce';
+var $list_limit = '20';
+var $legacy = '0';
+/* Debug Settings */
+var $debug = '0';
+var $debug_lang = '0';
+/* Database Settings */
+var $dbtype = 'mysql';
+var $host = 'localhost';
+var $user = 'apexsinc_baknudz';
+var $password = 'nsereno123jr';
+var $db = 'apexsinc_cardriverdata';
+var $dbprefix = 'jos_';
+/* Server Settings */
+var $secret = 'atg1FpKPObfw8pR2';
+var $gzip = '0';
+var $error_reporting = '-1';
+var $helpurl = 'http://help.joomla.org';
+var $xmlrpc_server = '1';
+var $ftp_host = '127.0.0.1';
+var $ftp_port = '21';
+var $ftp_user = '';
+var $ftp_pass = '';
+var $ftp_root = '';
+var $ftp_enable = '0';
+/* Locale Settings */
+var $offset = '0';
+var $offset_user = '0';
+/* Mail Settings */
+var $mailer = 'mail';
+var $mailfrom = 'nsereno@apexvalue.com';
+var $fromname = 'www.cardriverdata.com';
+var $sendmail = '/usr/sbin/sendmail';
+var $smtpauth = '0';
+var $smtpuser = '';
+var $smtppass = '';
+var $smtphost = 'localhost';
+/* Cache Settings */
+var $caching = '0';
+var $cachetime = '15';
+var $cache_handler = 'file';
+/* Meta Settings */
+var $MetaDesc = 'Motor Vehicle Speed Monitoring: Distributor - Developer of Microprocessor-computer-based products - Driveright 600e Series and DriveRight 600e Accessories, Carchip Pro and CarChip Fleet Pro vehicle monitoring devices, V-Count II, SpeedSpy and T.E.S.O.';
+var $MetaKeys = 'vehicle speed monitoring, Carchip Pro, CarChip Fleet Pro, DriveRight 600e, DriveRight, Drive Right, motor vehicle speed monitoring, motor vehicle speed monitor, vehicle speed monitor, car speed monitoring, car speed monitor, car speed watcher, v-count, carchip, driveright, DriveRight 600, teenage driving safety, teen drivers, expert systems, Teen Driving Information. Parents and Teen Drivers, teen driving, driving, teen driver, driver, teenager, teen, parents, automobile, driving instruction, instruction, safe driver, safe, fatality, automobile accident, accident, graduated driver license, license, teen driving instruction, instruction, free, training, manual, parenting, chart, advice, helpful hint, helptful, hints, driving skill, skill, safety, research, teens, information and solutions, information, solutions, resource, center, contract, agreement, teen driving, overspeeding, speeding ticket, reckless driving, defensive driving, automotive aftersales products, vehicle engine maintenance device, OBD II diagnostics, engine parameter monitoring, avoiding speeding tickets, skillful driving.';
+var $MetaTitle = '1';
+var $MetaAuthor = '1';
+/* SEO Settings */
+var $sef           = '0';
+var $sef_rewrite   = '0';
+var $sef_suffix    = '0';
+/* Feed Settings */
+var $feed_limit   = 10;
+var $log_path = '/usr/home/apexsinc/public_html/www.cardriverdata.com/logs';
+var $tmp_path = '/usr/home/apexsinc/public_html/www.cardriverdata.com/tmp';
+/* Session Setting */
+var $lifetime = '15';
+var $session_handler = 'database';
+}
+?>
